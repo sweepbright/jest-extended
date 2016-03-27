831ab3beaae0bce8678829b29d38e117d322db6a
Maxime Fabre <ehtnam6@gmail.com>
2016-03-27 11:57:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Madewithlove Karma configuration
+
+## Usage
+
+**karma.conf.js**
+```js
+var config = require('madewithlove-karma-config');
+
+module.exports = config;
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+// Karma configuration
+// Generated on Thu Mar 10 2016 14:10:35 GMT+0100 (CET)
+var webpack = require('webpack');
+
+module.exports = function (config) {
+    config.set({
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha'],
+
+        // list of files / patterns to load in the browser
+        files: [
+            'tests/index.js',
+        ],
+
+        exclude: [
+            'tests/_coverage/**/*',
+        ],
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'tests/index.js': 'webpack',
+        },
+
+        webpack: {
+            plugins: [
+                new webpack.DefinePlugin({
+                    __DEVTOOLS__: true,
+                }),
+            ],
+            module: {
+                loaders: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loaders: ['babel', 'isparta'],
+                    },
+                    {
+                        test: /\.json$/,
+                        loader: 'json',
+                    },
+                    {
+                        test: /\.(svg|css|scss|png)$/,
+                        loader: 'null',
+                    },
+                ],
+            },
+        },
+
+        webpackMiddleware: {
+            noInfo: true,
+        },
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['mocha', 'coverage'],
+
+        mochaReporter: {
+            showDiff: true,
+        },
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'tests/_coverage',
+        },
+
+        browsers: ['Chrome'],
+    });
+};
